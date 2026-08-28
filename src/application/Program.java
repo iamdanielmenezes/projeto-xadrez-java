5bd17e2734dbe17c0cf46b230a9004bc7b176ccc
Daniel Menezes <dfm.daniel.menezes@gmail.com>
2026-08-28 14:36:48 -0300

    Implementando board e piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position p = new Position(1,2);
-		System.out.println(p);
+		Board borad = new Board(8, 8);
 
 	}
 
---
+++ src/boardgame/Board.java
@@ -0,0 +1,30 @@
+package boardgame;
+
+public class Board {
+
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns]; //a matriz de peças vai ser instanciada com a quantidade de linhas e colunas informadas
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+}
