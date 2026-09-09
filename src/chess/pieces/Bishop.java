cce8faec99032a3e9d63e024add12873adcd3894
Daniel Menezes <dfm.daniel.menezes@gmail.com>
2026-09-08 21:23:29 -0300

    criando bispo

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece{ // classe "bispo"
+	
+	public Bishop(Board board, Color color) {
+		super(board, color); 
+	}
+
+	@Override
+	public String toString() {
+		return "B"; 
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; //matriz booleana com as dimenções do tabuleiro
+		
+		Position p = new Position(0, 0);
+		
+		//marcar as posições de verdadeiro na diagonal pra cima a esquerda
+		p.setValues(position.getRow() -1, position.getColumn() -1); //pega a posição da peça menos 1, que seria a posição de cima na matriz
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {//enquanto a posição existir e tiver vaga, ela vai ser verdadeira
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1); 
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//marcar as posições de verdadeiro na diagonal pra cima a direita
+		p.setValues(position.getRow() - 1, position.getColumn() +1 ); //pega a posição da peça menos 1, que seria a posição a esquerda na matriz
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {//enquanto a posição existir e tiver vaga, ela vai ser verdadeira
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//marcar as posições de verdadeiro na diagonal pra baixo a direita
+		p.setValues(position.getRow() + 1, position.getColumn() +1 ); //pega a posição da peça mais 1, que seria a posição a direita na matriz
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {//enquanto a posição existir e tiver vaga, ela vai ser verdadeira
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//marcar as posições de verdadeiro na diagonal pra baixo a esquerda
+				p.setValues(position.getRow() +1, position.getColumn() - 1); //pega a posição da peça mais 1, que seria a posição de baixo na matriz
+				while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {//enquanto a posição existir e tiver vaga, ela vai ser verdadeira
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() + 1, p.getColumn() - 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+		
+		return mat;
+	}
+}
